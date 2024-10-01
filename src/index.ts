43b7b4b118112259a283efc90ea06a8812f02a70
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-10-01 09:37:56 +0700

    refactor: replace getAppName with package-metadata

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import path from "node:path";
 import * as codegen from "@omer-x/openapi-code-generator";
-import getAppName from "./core/app";
+import getPackageMetadata from "@omer-x/package-metadata";
 import getArgument from "./core/arguments";
 import capitalize from "./core/capitalize";
 import createFile from "./core/file";
@@ -30,8 +30,7 @@ import fetchOpenApiSpec from "./core/spec";
     }
   }
 
-  const packageName = getAppName();
-  const appName = packageName.split("/").pop() ?? "unknown-service";
+  const { packageName, moduleName: appName } = getPackageMetadata();
   const serviceName = capitalize(appName.replace(/-/g, " "));
   const envName = `${appName.replace(/-/g, "_").toUpperCase()}_BASE_URL`;
 
