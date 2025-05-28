90c35ce3b39c7878912bb7b1eef8877f98617f68
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2025-05-28 07:40:34 +0700

    feat: fetching local openapi specs

--- src/core/fetchOpenApiSpec.ts
+++ src/core/fetchOpenApiSpec.ts
@@ -1,6 +1,14 @@
+import fs from "node:fs/promises";
+import path from "node:path";
 import type { OpenApiDocument } from "@omer-x/openapi-types";
 
-export default async function fetchOpenApiSpec(sourceURL: string) {
-  const response = await fetch(sourceURL);
-  return await response.json() as OpenApiDocument;
+export default async function fetchOpenApiSpec(source: string) {
+  if (source.startsWith("http://") || source.startsWith("https://")) {
+    const response = await fetch(source);
+    return (await response.json()) as OpenApiDocument;
+  } else {
+    const absolutePath = path.resolve(source);
+    const fileContent = await fs.readFile(absolutePath, "utf-8");
+    return JSON.parse(fileContent) as OpenApiDocument;
+  }
 }
